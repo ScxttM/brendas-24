cdf7eed355c2858bef9c3646163140e83fcc279c
ScxttM <alan.montesinos@uabc.edu.mx>
2024-10-03 00:23:24 -0700

    first commit

---
+++ src/components/Navbar.tsx
@@ -0,0 +1,15 @@
+const Navbar = () => {
+  return (
+    <div className="navbar bg-base-100 text-primary mt-8 px-9">
+      <div className="navbar-start"></div>
+      <div className="navbar-center">
+        <a className="text-3xl" href="#">
+          Brenda's 24th
+        </a>
+      </div>
+      <div className="navbar-end text-2xl">October 11th</div>
+    </div>
+  );
+};
+
+export default Navbar;
